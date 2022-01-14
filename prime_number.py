e62d604799e621f653ac8483058c19a372eba897
vihaanmiriyala <vihaannagmiriyala@gmail.com>
2022-01-13 19:27:41 -0800

    Validate a number is prime or not

--- prime_number.py
+++ prime_number.py
@@ -1,13 +1,12 @@
-#a prime number is a number that has factors of 1 and itself
 
-input_number = 21
+
+input_number = 1
 is_prime = True
 
 
-for i in range(2, input_number): #loop tells what numbers have to be pecercented by input number, i is a variable
+for i in range(2, input_number):
     if input_number % i == 0:
-        #this tells the computer that if input_number percented by i equals to 0,the number will not be prime
-        is_prime = False #this assigns the value of the variable is_prime, "False"
-        break #break command
+        is_prime = False
+        break
 
-print(is_prime) #this prints is_prime
+print(is_prime)
